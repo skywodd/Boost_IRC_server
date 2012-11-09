a6d457c7eedbb22755f8360de6f4e9f44be95f97
Skywodd <skywodd@gmail.com>
2012-11-09 22:59:48 +0100

    Beta - stable version

--- Debug_log.hpp
+++ Debug_log.hpp
@@ -1,17 +1,17 @@
 /**
  * @file Debug_log.hpp
- * @brief Debug logging utility - template header
+ * @brief Debug log utility
  * @author SkyWodd
  * @version 1.0
  * @see http://skyduino.wordpress.com/
  *
  * @section intro_sec Introduction
- * This header is a container for various debug utilities (template functions).\n
+ * This header is a functions bundle of various debug utilities (template functions).\n
  * They have been coded to be generic and easily extensible.\n
  * \n
  * Please report bug to <skywodd at gmail.com>
  *
- * @section licence_sec Licence
+ * @section licence_sec License
  *  This program is free software: you can redistribute it and/or modify\n
  *  it under the terms of the GNU General Public License as published by\n
  *  the Free Software Foundation, either version 3 of the License, or\n
@@ -27,26 +27,28 @@
  *
  * @section other_sec Others notes and compatibility warning
  * Require a C++ compiler with templates support.\n
- * Use clog as output.
+ * Use clog as output (can be easily redirected to a file).
  */
 
 #ifndef DEBUG_LOG_H_
 #define DEBUG_LOG_H_
 
-/* Dependency */
+/* Dependencies */
 #include <iostream>
 
 /**
  * @namespace debug
+ *
+ * Namespace regrouping all debug utilities.
  */
 namespace debug {
 
 /**
- * Output variable to debug log stream (using ostream << operator overload)
+ * Output variable to debug log stream (using << operator overload)
  *
- * @pre Target variable MUST define << operator overload to ostream !
- * @remark Use template for genericity
- * @remark Define/undefine DEBUG to enable/disable debug output
+ * @pre Target variable MUST define << operator overload !
+ * @remark Define / undefine DEBUG to enable / disable debug output
+ *
  * @param var Target variable to display
  */
 #ifdef DEBUG
@@ -61,12 +63,12 @@ inline void DEBUG_LOG(const T& var) {
 #endif
 
 /**
- * Output variable to debug log stream (using ostream << operator overload)
+ * Output variable to debug log stream (using << operator overload)
  *
- * @pre Target variable MUST define << operator overload to ostream !
- * @remark Use template for genericity
- * @remark Define/undefine DEBUG to enable/disable debug output
- * @param str Label to display before variable
+ * @pre Target variable MUST define << operator overload !
+ * @remark Define / undefine DEBUG to enable / disable debug output
+ *
+ * @param str Label to display before the variable
  * @param var Target variable to display
  */
 #ifdef DEBUG
@@ -81,12 +83,12 @@ inline void DEBUG_LOG(const std::string& str, const T& var) {
 #endif
 
 /**
- * Output variable to debug log stream (using ostream << operator overload)
+ * Output variable to debug log stream (using << operator overload)
  *
- * @pre Target variable MUST define << operator overload to ostream !
- * @remark Use template for genericity
- * @remark Define/undefine DEBUG to enable/disable debug output
- * @param str Label to display before variable
+ * @pre Target variable MUST define << operator overload !
+ * @remark Define / undefine DEBUG to enable / disable debug output
+ *
+ * @param str Label to display before the variable
  * @param key Target variable key to display
  * @param value Target variable value to display
  */
@@ -101,6 +103,6 @@ inline void DEBUG_LOG(const std::string& str, const std::string& key, const T& v
 }
 #endif
 
-}
+} /* namespace debug */
 
 #endif /* DEBUG_LOG_H_ */
